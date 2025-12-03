cd74409cbc1e843679919bbcc4bd3bedea5adebd
Enrique Luviano (chihuahuaLover69) <kikeluviano1810@gmail.com>
2025-12-02 19:48:17 -0600

    App.js
    Main bot script

---
+++ app.js
@@ -0,0 +1,121 @@
+async function httpGetAsync(theUrl) {
+  const login = {
+    "email": "briankalid@comunidad.unam.mx",
+    "password": "securepassword"
+  };
+
+  const JsonString = JSON.stringify(login);
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JsonString
+  };
+
+  const response = await fetch(theUrl, options);
+  
+  if (!response.ok) {
+    throw new Error("Error en la petición: " + response.status);
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+
+
+
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const WebWhatsappProvider = require('@bot-whatsapp/provider/web-whatsapp')
+const JsonFileAdapter = require('@bot-whatsapp/database/json')
+
+const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+
+const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
+    [
+        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
+        'https://bot-whatsapp.netlify.app/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+    [
+        '🙌 Aquí encontras un ejemplo rapido',
+        'https://bot-whatsapp.netlify.app/docs/example/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
+    [
+        '🚀 Puedes aportar tu granito de arena a este proyecto',
+        '[*opencollective*] https://opencollective.com/bot-whatsapp',
+        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
+        '[*patreon*] https://www.patreon.com/leifermendez',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowDiscord = addKeyword(['discord']).addAnswer(
+    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+    .addAnswer(
+        [
+            'te comparto los siguientes links de interes sobre el proyecto',
+            '👉 *doc* para ver la documentación',
+            '👉 *gracias*  para ver la lista de videos',
+            '👉 *discord* unirte al discord',
+        ],
+        null,
+        null,
+        [flowDocs, flowGracias, flowTuto, flowDiscord]
+    )
+    .addAction(async(ctx) => {
+
+
+        console.log(`que royo, plebe 2`)
+
+    })
+    .addAction(async (ctx) => {
+    try {
+        const respuesta = await httpGetAsync("http://127.0.0.1:8000/v1/login");//http://127.0.0.1:8000/v1/ticket/14e3ba77-9656-4ade-b04b-5fe84a7f707e
+        
+        console.log("La respuesta fue:", respuesta);
+    } catch (err) {
+        console.error(err);
+    }
+})
+const main = async () => {
+    const adapterDB = new JsonFileAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(WebWhatsappProvider)
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+    QRPortalWeb()
+}
+
+main()
+console.log(`que royo, plebe`)
